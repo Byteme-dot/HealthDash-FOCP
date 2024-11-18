fcdb0d99f18309e2b21f89abd185911aa509174c
Byteme-dot <124488563+Byteme-dot@users.noreply.github.com>
2024-11-18 13:43:21 +0530

    Improved
    Okay guys, hello,

--- Main.c
+++ Main.c
@@ -3,14 +3,15 @@
 void user_input(void);
 void user_info_print(void);
 typedef struct{
+    int day;
     int weight;
-    int height;
     int calorie_burn;
     int calorie_intake;
 }User;
 User days[30];
 char name[30];
 int age;
+int height;
 int global_choice;
 int main(){
     user_input();
@@ -25,25 +26,28 @@ void user_input(){
     scanf(" %s",&name);
     printf("Enter your age: ");
     scanf(" %d",&age);
-    printf("Enter no of days: ");
+    printf("Enter your height: ");
+    scanf(" %d",&height);
+    printf("Enter no of days to input data: ");
     scanf("%d",&choice);
     global_choice=choice;
-    fprintf(fptr,"Name: %s \nAge=%d\n",name,age);
+    fprintf(fptr,"Name: %s \nAge=%dY \nHeight=%dcm\n",name,age,height);
     for (int i=0;i<choice;i++){
-        printf("Enter your height: ");
-        scanf(" %d",&days[i].height);
+        printf("*****DAY %d*****\n",i+1);
+        days[i].day=i+1;
         printf("Enter your weight: ");
         scanf(" %d",&days[i].weight);
         printf("Enter your calories burnt: ");
         scanf(" %d",&days[i].calorie_burn);
         printf("Enter your calorie intake: ");
         scanf(" %d",&days[i].calorie_intake);
-        fprintf(fptr,"Day%d : Height=%d - Weight=%d - Calorie Burnt=%d - Calorie Intake=%d \n",global_choice+1,days[i].height,days[i].weight,days[i].calorie_burn,days[i].calorie_intake);
+        fprintf(fptr,"Day%d : Weight=%d - Calorie Burnt=%d - Calorie Intake=%d \n",days[i].day,days[i].weight,days[i].calorie_burn,days[i].calorie_intake);
     }
     fclose(fptr);
+    free(fptr);
 }
 void user_info_print(){
     for(int i=0;i<global_choice;i++){
-        printf("Day%d : Height=%d - Weight=%d - Calorie Burnt=%d - Calorie Intake=%d \n",global_choice+1,days[i].height,days[i].weight,days[i].calorie_burn,days[i].calorie_intake);
+        printf("Day%d : Weight=%d - Calorie Burnt=%d - Calorie Intake=%d \n",days[i].day,days[i].weight,days[i].calorie_burn,days[i].calorie_intake);
     }
 }
